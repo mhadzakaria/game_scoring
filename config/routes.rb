9c973fdcc87ce9482d93998d46b82d2b9232e2c1
3lviend <henryss_10@yahoo.com>
2020-09-22 14:52:20 +0700

    root path, schema updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :remi_games
+
+  root 'remi_games#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
